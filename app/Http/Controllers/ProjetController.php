397bd9816f8fe82cb0a1106bb5ce787b1d3a3b7a
Owolabi <lawaleulysse@gmail.com>
2021-06-17 16:07:08 +0300

    Fixed typo in file upload

--- app/Http/Controllers/ProjetController.php
+++ app/Http/Controllers/ProjetController.php
@@ -47,28 +47,10 @@ class ProjetController extends Controller
                 'File.*' => 'mimes:jpeg,jpg,png,gif,csv,txt,pdf,zip,docx'
               ]);
           
-              if($request->hasfile('File')) {
+              if($request->hasfile('File')) 
+              {
 
-            //     foreach($request->file('File') as $file)
-            //     {
-            //         $name = $file->getClientOriginalName();
-            //         $file->move(public_path().'/files/', $name);  
-            //         $Data[] = $name;  
-            //     }
-
-            //     foreach ($Data as $fileModal ) {
-            //         # code...
-               
-        
-            //     $fileModal = new Fichier();
-            //     $fileModal->nom = json_encode($Data);
-            //     $fileModal->extension = "png";
-            //     $fileModal->user_id = Auth::id();
-                
-               
-            //     $fileModal->save();
-            // }
-                
+           
       
                   foreach($request->file('File') as $file)
                   {
@@ -126,6 +108,64 @@ class ProjetController extends Controller
         }
     else
     {
+        if($request->hasfile('File')) 
+        {
+
+     
+
+            foreach($request->file('File') as $file)
+            {
+                  $fileModal = new Fichier();
+
+                $filenameWithExt = $file->getClientOriginalName();
+                // Get just filename
+                $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+                // Get just ext
+                $extension = $file->getClientOriginalExtension();
+                // Filename to store
+                $fileNameToStore= $filename.'_'.time().'.'.$extension;
+                // Upload Image
+                $path = $file->storeAs('public/files', $fileNameToStore);
+
+           
+                $fileModal->nom = $fileNameToStore;
+                $fileModal->extension = $extension;
+                $fileModal->user_id = Auth::id();
+            
+                $fileModal->save();
+              }
+
+              
+                  $fdate = $request->debut;
+                  $tdate = $request->fin;
+                  $datetime1 = new \DateTime($fdate);
+                  $datetime2 = new \DateTime($tdate);
+                  $interval = $datetime1->diff($datetime2);
+
+              
+              $projet = Projet::create([
+                      'nom' => $request->nom,
+                      'description' => $request->description,
+                      'duree' => $interval->days,
+                      'debut' => $request->debut,
+                      'fin' => $request->fin,
+                      'filiere_id' => $request->filiere,
+                      'user_id' => Auth::id(),
+                  ]);
+
+                  $files = Fichier::where('user_id',Auth::id())->get();
+
+                  foreach ($files as $file ) 
+                  {
+                    $file->projet_id = $projet->id;
+                    $file->save();
+                  }
+
+
+    
+            return back()->with('success', 'Creation du projet réussi !');
+        }
+
 
    
 
