ec7fb7747f4f4d884b49d699bfd237f431b02b79
naja456 <habibabdoulkoudouss@gmail.com>
2021-06-22 10:36:05 +0200

    ajout login home register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="signup__container2">
+<div class="signup__container23">
 
+    <form action="#">
+        
             <div class="flash-message">
                 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
                     @if(Session::has('alert-' . $msg))
@@ -12,18 +13,16 @@
                     @endif
                 @endforeach
             </div>
-
-            <div class="card">
                 <div class="card-header">{{ __('Connexion') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __("Addresse email de l'efrei") }}</label>
+                        <div class="form-group">
+                            <label for="email" >{{ __(" Email  ") }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -34,10 +33,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
+                        <div class="form-group">
+                            <label for="password" >{{ __('Mot de passe') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -48,7 +47,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group ">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -60,7 +59,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group ">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Connexion') }}
@@ -68,7 +67,7 @@
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Se souvenir de moi ?') }}
+                                        Mot de Passe oublié ?
                                     </a>
                                 @endif
                             </div>
@@ -78,5 +77,7 @@
             </div>
         </div>
     </div>
+    </div>
 </div>
+
 @endsection
